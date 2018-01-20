6e34c1f3fb36a23be2307296861ea0ec7a708a92
Feng Ju <RichieJu520@users.noreply.github.com>
2018-01-20 19:58:52 +0100

    Update 1.Pairwise_correlations.R

--- 1.Pairwise_correlations.R
+++ 1.Pairwise_correlations.R
@@ -1,7 +1,7 @@
-###@author: Feng Ju
-###@email: richieju520@gmail.com
+###@author Feng Ju
+###@email richieju520@gmail.com
 
-################## Correlation analysis ###############################
+################## C-score calculation and correlation analysis ###############################
 #install.packages("vegan")
 #install.packages("igraph")
 #install.packages("Hmisc")
@@ -11,6 +11,9 @@ library(igraph)
 library(Hmisc)
 
 co_occurrence_network<-function(matrix,alpha,p.cutoff){
+  
+  matrix1<-matrix
+  matrix1[matrix1>0]<-1
   
   #correlation analysis based on spearman's co-efficient
   matrix.dist<-rcorr(t(matrix),type="spearman")
@@ -25,8 +28,7 @@ co_occurrence_network<-function(matrix,alpha,p.cutoff){
   matrix.cor1.p<-matrix.cor.p
   matrix.cor1[which(matrix.cor1 <= alpha)]=0
   matrix.cor1[which(matrix.cor1.p>p.cutoff)]=0
-
-  # drop those rows and columns with sum = 0
+  # delete those rows and columns with sum = 0
   matrix.cor1<-matrix.cor1[which(rowSums(matrix.cor1)!=1),]
   matrix.cor1<-matrix.cor1[,which(colSums(matrix.cor1)!=0)]
   
@@ -35,9 +37,8 @@ co_occurrence_network<-function(matrix,alpha,p.cutoff){
   ###matrix.cor2.p<-matrix.cor.p
   ###matrix.cor2[which(matrix.cor2 > (-alpha))]=0
   ###matrix.cor2[which(matrix.cor2.p>p.cutoff)]=0
-
-  # drop those rows and columns with sum = 0
-  ###matrix.cor2<-matrix.cor2[which(rowSums(matrix.cor2)!=1),]
+  # delete those rows and columns with sum = 0
+  ###matrix.cor2<-matrix.cor2[which(rowSums(matrix.cor2)!=0),]
   ###matrix.cor2<-matrix.cor2[,which(colSums(matrix.cor2)!=0)]
   
   #3.Consider both positive and netagive cooccurence at given coefficient (alpha) and p-value cutoffs
@@ -45,12 +46,12 @@ co_occurrence_network<-function(matrix,alpha,p.cutoff){
   matrix.cor3.p<-matrix.cor.p
   matrix.cor3[which(matrix.cor3>=(-alpha) & matrix.cor3 <= alpha)]=0
   matrix.cor3[which(matrix.cor3.p>p.cutoff)]=0
-
-  # drop those rows and columns with sum = 0
+  
+  # delete those rows and columns with sum = 0
   matrix.cor3<-matrix.cor3[which(rowSums(matrix.cor3)!=1),]
   matrix.cor3<-matrix.cor3[,which(colSums(matrix.cor3)!=0)]
   
-  # generate graphs using igraph
+  # generate graph using igraph
   g1<-graph.adjacency(matrix.cor1,weight=T,mode="undirected")
   g1<-simplify(g1)
   V(g1)$label <- V(g1)$name
@@ -68,7 +69,7 @@ co_occurrence_network<-function(matrix,alpha,p.cutoff){
   
   # append the output into results
   result<-list()
-  
+  #result$co_occurrence<-co_occurrence
   result$matrix.cor<-matrix.cor
   result$matrix.cor.p<-matrix.cor.p
   
