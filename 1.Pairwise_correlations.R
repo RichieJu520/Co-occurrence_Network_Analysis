b625af9c131997421bdca40766904f3d7de04d8a
Feng Ju <RichieJu520@users.noreply.github.com>
2018-01-20 20:23:15 +0100

    Update 1.Pairwise_correlations.R

--- 1.Pairwise_correlations.R
+++ 1.Pairwise_correlations.R
@@ -1,7 +1,7 @@
 ###@author Feng Ju
 ###@email richieju520@gmail.com
 
-################## C-score calculation and correlation analysis ###############################
+################## Correlation analysis ###############################
 #install.packages("vegan")
 #install.packages("igraph")
 #install.packages("Hmisc")
@@ -10,41 +10,42 @@ library(vegan)
 library(igraph)
 library(Hmisc)
 
-co_occurrence_network<-function(matrix,alpha,p.cutoff){
+co_occurrence_network<-function(matrix,cor.cutoff,p.cutoff){
   
   matrix1<-matrix
   matrix1[matrix1>0]<-1
   
   #correlation analysis based on spearman's co-efficient
   matrix.dist<-rcorr(t(matrix),type="spearman")
+  ###matrix.dist<-rcorr(t(matrix),type="pearson")
   matrix.cor<-matrix.dist$r
   matrix.cor.p<-matrix.dist$P
   
   #Multiple testing correction using Benjamini-Hochberg standard false discovery rate correction ("FDR-BH")
   matrix.cor.p <- p.adjust(matrix.cor.p, method="BH")
   
-  #1.Consider positive cooccurence at given coefficient (alpha) and p-value cutoffs
+  #1.Consider positive cooccurence at given coefficient (cor.cutoff) and p-value cutoffs
   matrix.cor1<-matrix.cor
   matrix.cor1.p<-matrix.cor.p
-  matrix.cor1[which(matrix.cor1 <= alpha)]=0
+  matrix.cor1[which(matrix.cor1 <= cor.cutoff)]=0
   matrix.cor1[which(matrix.cor1.p>p.cutoff)]=0
   # delete those rows and columns with sum = 0
   matrix.cor1<-matrix.cor1[which(rowSums(matrix.cor1)!=1),]
   matrix.cor1<-matrix.cor1[,which(colSums(matrix.cor1)!=0)]
   
-  #2.Consider netagive cooccurence at given coefficient (-alpha) and p-value cutoffs
+  #2.Consider netagive cooccurence at given coefficient (-cor.cutoff) and p-value cutoffs
   ###matrix.cor2<-matrix.cor
   ###matrix.cor2.p<-matrix.cor.p
-  ###matrix.cor2[which(matrix.cor2 > (-alpha))]=0
+  ###matrix.cor2[which(matrix.cor2 > (-cor.cutoff))]=0
   ###matrix.cor2[which(matrix.cor2.p>p.cutoff)]=0
   # delete those rows and columns with sum = 0
   ###matrix.cor2<-matrix.cor2[which(rowSums(matrix.cor2)!=0),]
   ###matrix.cor2<-matrix.cor2[,which(colSums(matrix.cor2)!=0)]
   
-  #3.Consider both positive and netagive cooccurence at given coefficient (alpha) and p-value cutoffs
+  #3.Consider both positive and netagive cooccurence at given coefficient (cor.cutoff) and p-value cutoffs
   matrix.cor3<-matrix.cor
   matrix.cor3.p<-matrix.cor.p
-  matrix.cor3[which(matrix.cor3>=(-alpha) & matrix.cor3 <= alpha)]=0
+  matrix.cor3[which(matrix.cor3>=(-cor.cutoff) & matrix.cor3 <= cor.cutoff)]=0
   matrix.cor3[which(matrix.cor3.p>p.cutoff)]=0
   
   # delete those rows and columns with sum = 0
@@ -82,4 +83,4 @@ co_occurrence_network<-function(matrix,alpha,p.cutoff){
   result$matrix.cor3<-matrix.cor3
   result$graph3<-g3
   return(result)
-} 
+}
